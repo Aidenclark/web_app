639ba7f601d8dd4a41ffa12a574da87535b7ee33
Aiden Clark <aidenhclark@gmail.com>
2023-01-30 20:56:20 -0600

    init

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Nav bar???
+        </div>
+      </div>
+
+
+
+
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          everything that will be visible on the page
+        </div>
+      </div>
+
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats 
+          poopsock
+          morepoop
+          
+        </div>
+      </div>
+
+
+
+
+
+    </div>
   )
 }
+
 
 export default App
